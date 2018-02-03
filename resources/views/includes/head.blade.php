7183a6deeb60d11615adb245c68a98ebde03c6b2
Gaëtan REBUT <gaetan.rebut@gmail.com>
2018-02-03 18:51:04 +0100

    Cache auto refresh (timestamps)

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -25,7 +25,7 @@
 </script>
 
 @section('css')
-<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+<link rel="stylesheet" href="{{ elixir('css/app.css') }}{{(config('app.env')=='local')?'?'.filemtime(public_path().'/css/app.css'):''}}">
 <link rel="icon" type="image/png" href="img/favicon.png" />
 <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" /><![endif]-->
 <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
