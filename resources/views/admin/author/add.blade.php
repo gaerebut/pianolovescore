4d9ec35eddcb234eca954cff5af531007464f245
Gaëtan REBUT <gaetan.rebut@gmail.com>
2019-08-27 18:29:16 +0200

    Prepare to HTTPS URL

--- resources/views/admin/author/add.blade.php
+++ resources/views/admin/author/add.blade.php
@@ -66,7 +66,7 @@
 @endsection
 @section('js_code')
 	@parent
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
 	<script src="{{ elixir( '/js/sanitize.js' ) }}"></script>
 
 	<script type="text/javascript">
