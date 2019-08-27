e097533f8f7ab6319abf26b1ed7d606b723859d8
Gaëtan REBUT <gaetan.rebut@gmail.com>
2019-08-27 23:01:49 +0200

    Sitemap homonyme categories hook

--- resources/views/public/sitemap/index.blade.php
+++ resources/views/public/sitemap/index.blade.php
@@ -1,7 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
-        <loc>{{ route(__('routes.sitemap_category')) }}</loc>
+        <loc>{{ route(__('routes.sitemap_categories')) }}</loc>
         <lastmod>{{ $score->updated_at->tz('UTC')->toAtomString() }}</lastmod>
     </sitemap>
     <sitemap>
