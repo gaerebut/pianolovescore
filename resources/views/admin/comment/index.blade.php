24a853a8ca25c2a13c0688142db154e390d7e57c
Gaëtan REBUT <gaetan.rebut@gmail.com>
2019-09-18 14:24:08 +0200

    Admin > Comment : Add Date to listing

--- resources/views/admin/comment/index.blade.php
+++ resources/views/admin/comment/index.blade.php
@@ -9,6 +9,7 @@
 			<thead>
 				<tr>
 					<th></th>
+					<th>Datew/th>
 					<th width="30%">Commentaire</th>
 					<th>Pseudo</th>
 					<th>Titre</th>
@@ -21,6 +22,11 @@
 					@foreach($new_comments as $comment)
 						<tr>
 							<td><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span></td>
+							<td>@if($comment->score)
+									$comment->score->created_at->formatLocalized('%d/%m/%Y')
+								@elseif($comment->trick)
+									$comment->trick->created_at->formatLocalized('%d/%m/%Y')
+								@endif</td>
 							<td>{{ $comment->comment }}</td>
 							<td>{{ $comment->username }}</td>
 							<td>
